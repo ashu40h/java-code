a127684acb1f01cb9ede15b2141881ee65e55430
Sharma Aashu <157596646+ashu40h@users.noreply.github.com>
2024-12-10 00:03:29 +0530

    Create checkVowelInAString.java

---
+++ checkVowelInAString.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+    String name = "ashu";
+    
+    name.toLowerCase();
+    
+    if(name.contains("a")||name.contains("i")||name.contains("o")
+    ||name.contains("u")){
+     System.out.print(name+ " containing vowel ");
+     }else{
+         System.out.print(name+ "is not containing vowel ");
+    }
+}
+}
